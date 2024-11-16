3be21ccd2ab0823cbc27721f073831221aa13c9b
Arshan S <arenwreck@gmail.com>
2024-11-16 08:48:45 +0530

    removed comments

--- .12.cpp
+++ .12.cpp
@@ -42,33 +42,28 @@ using namespace std;
 
 int main()
 {
-    // creating socket
+    
     int serverSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    // specifying the address
+ 
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    // binding socket.
-    bind(serverSocket, (struct sockaddr*)&serverAddress,
-         sizeof(serverAddress));
+    bind(serverSocket, (struct sockaddr*)&serverAddress,sizeof(serverAddress));
 
-    // listening to the assigned socket
+   
     listen(serverSocket, 5);
 
-    // accepting connection request
-    int clientSocket
-        = accept(serverSocket, nullptr, nullptr);
+    int clientSocket  = accept(serverSocket, nullptr, nullptr);
 
-    // recieving data
+ 
     char buffer[1024] = { 0 };
     recv(clientSocket, buffer, sizeof(buffer), 0);
-    cout << "Message from client: " << buffer
-              << endl;
+    cout << "Message from client: " << buffer<< endl;
 
-    // closing the socket.
+    
     close(serverSocket);
 
     return 0;
@@ -90,24 +85,22 @@ RECIEVER
 
 int main()
 {
-    // creating socket
+  
     int clientSocket = socket(AF_INET, SOCK_STREAM, 0);
 
-    // specifying address
+    
     sockaddr_in serverAddress;
     serverAddress.sin_family = AF_INET;
     serverAddress.sin_port = htons(8080);
     serverAddress.sin_addr.s_addr = INADDR_ANY;
 
-    // sending connection request
+    
     connect(clientSocket, (struct sockaddr*)&serverAddress,
             sizeof(serverAddress));
 
-    // sending data
     const char* message = "Hello, server!";
     send(clientSocket, message, strlen(message), 0);
 
-    // closing socket
     close(clientSocket);
 
     return 0;
